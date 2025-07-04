c5b8952861997c587d0e1885ebc255d88cfed3a5
woojin <77970494+woojin966@users.noreply.github.com>
2025-07-04 21:35:53 +0900

    footer 마크업과 대강의 스타일링

--- vueFlix/src/components/Footer.vue
+++ vueFlix/src/components/Footer.vue
@@ -1,5 +1,102 @@
 <template>
-    <span>ddddd</span>
+    <article class="footer_wrap">
+        <section class="footer_top">
+            <ul class="sns_box">
+                <li>
+                    <a href="javascript:void(0)" class="sns_btn">
+                        <font-awesome-icon :icon="['fab', 'facebook-f']" />
+                    </a>
+                </li>
+                <li>
+                    <a href="javascript:void(0)" class="sns_btn">
+                        <font-awesome-icon :icon="['fab', 'instagram']" />
+                    </a>
+                </li>
+                <li>
+                    <a href="javascript:void(0)" class="sns_btn">
+                        <font-awesome-icon :icon="['fab', 'x-twitter']" />
+                    </a>
+                </li>
+                <li>
+                    <a href="javascript:void(0)" class="sns_btn">
+                        <font-awesome-icon :icon="['fab', 'youtube']" />
+                    </a>
+                </li>
+            </ul>
+            <ul class="footer_menu_box">
+                <li>
+                    <a href="javascript:void(0)" class="f_menu_btn small n">화면 해설</a>
+                </li>
+                <li>
+                    <a href="javascript:void(0)" class="f_menu_btn small n">고객 센터</a>
+                </li>
+                <li>
+                    <a href="javascript:void(0)" class="f_menu_btn small n">기프트카드</a>
+                </li>
+                <li>
+                    <a href="javascript:void(0)" class="f_menu_btn small n">미디어 센터</a>
+                </li>
+                <li>
+                    <a href="javascript:void(0)" class="f_menu_btn small n">투자 정보(IR)</a>
+                </li>
+                <li>
+                    <a href="javascript:void(0)" class="f_menu_btn small n">입사 정보</a>
+                </li>
+                <li>
+                    <a href="javascript:void(0)" class="f_menu_btn small n">이용 약관</a>
+                </li>
+                <li>
+                    <a href="javascript:void(0)" class="f_menu_btn small n">개인정보</a>
+                </li>
+                <li>
+                    <a href="javascript:void(0)" class="f_menu_btn small n">법적 고지</a>
+                </li>
+                <li>
+                    <a href="javascript:void(0)" class="f_menu_btn small n">쿠키 설정</a>
+                </li>
+                <li>
+                    <a href="javascript:void(0)" class="f_menu_btn small n">회사 정보</a>
+                </li>
+                <li>
+                    <a href="javascript:void(0)" class="f_menu_btn small n">문의하기</a>
+                </li>
+            </ul>
+        </section>
+        <section class="footer_bottom">
+            <div class="servcicecod_btn_box">
+                <button type="button" class="servicecode_btn small n">서비스 코드</button>
+            </div>
+            <div class="infomation_box">
+                <p class="mini n">
+                    <span>뷰플릭스서비시스코리아 유한회사</span>
+                    <span>통신판매업신고번호 : 제0000-서울양천-0000호</span>
+                    <span>전화번호 : 00-000-000-0000(수신자 부담)</span>
+                </p>
+                <p class="mini n">
+                    <span>대표 : 예지킴</span>
+                </p>
+                <p class="mini n">
+                    <span>이메일 : woojin_966@naver.com</span>
+                </p>
+                <p class="mini n">
+                    <span>주소 : 00000, 대한민국 서울특별시 양천구 @@로 nn</span>
+                </p>
+                <p class="mini n">
+                    <span>사업자등록번호 : 000-00-00000</span>
+                </p>
+                <p class="mini n">
+                    <span>클라우드 호스팅 : Amazon Web Services Inc.</span>
+                </p>
+                <p class="mini n">
+                    <span>공정거래위원회 웹사이트</span>
+                </p>
+            </div>
+        </section>
+    </article>
 </template>
 
 <script setup></script>
+
+<style scoped lang="scss">
+@import '../assets/footer.scss';
+</style>
--- vueFlix/vite.config.js
+++ vueFlix/vite.config.js
@@ -4,4 +4,17 @@ import vue from '@vitejs/plugin-vue'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `@import "@/assets/_variable.scss";`
+      }
+    }
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+  base: '/vueFlix/',
 })
