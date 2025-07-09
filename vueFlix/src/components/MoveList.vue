918c89f63e4fb115831c58752d9661c8155aea3b
woojin <77970494+woojin966@users.noreply.github.com>
2025-07-09 16:16:45 +0900

    영화 목록 뿌리기

--- vueFlix/src/components/MoveList.vue
+++ vueFlix/src/components/MoveList.vue
@@ -1,35 +1,68 @@
 <template>
     <ul>
-        <li v-for="movie in filterMovies" :key="movie.title">
-            {{ movie.title }}
-        </li>
+        <MovieItem 
+            v-for="movie in filterMovies" 
+            :key="movie.id" 
+            :movie="movie"
+        ></MovieItem>
     </ul>
 </template>
 
 <script setup>
-    import { ref, computed } from 'vue'
+    import { ref, computed, onMounted, onBeforeUnmount } from 'vue'
+    import axios from 'axios'
     import Hangul from 'hangul-js'
+    import MovieItem from './MovieItem.vue'
     
     const props = defineProps(['keyword'])
+    let page = ref(1)
+    const isFetching = ref(false)
+    const movies = ref([])
 
-    const movies = [
-        { title: '인셉션' },
-        { title: '인터스텔라' },
-        { title: '다크나이트' },
-        { title: '아이언맨' },
-        { title: 'Superman' },
-        { title: 'Anne with an E' },
-        { title: 'Gossip girl' },
-        { title: 'Life is go on with tanserine' },
-    ]
+    const fetchMovies = async () => {
+        if (isFetching.value) return
+        isFetching.value = true
+
+        try{
+            const movieRes  = await axios.get('https://api.themoviedb.org/3/discover/movie', {
+                params: {
+                    api_key: 'fcc17cdaa94f35e5e1cf80b2de9ea4e7',
+                    language: 'ko-KR',
+                    sort_by: 'popularity.desc',
+                    page: page.value
+                }
+            })
+            movies.value.push(...movieRes.data.results)
+        } finally {
+            isFetching.value = false
+        }
+    }
+
+    onBeforeUnmount(() => {
+        window.removeEventListener('scroll', handleScroll)
+    })
+
+    onMounted(() => {
+        fetchMovies()
+        window.addEventListener('scroll', handleScroll)
+    })
+
+    const handleScroll = () => {
+        const scrollHeight = document.documentElement.scrollHeight
+        const scrollTop = document.documentElement.scrollTop
+        const clientHeight = document.documentElement.clientHeight
+
+        if (scrollTop + clientHeight >= scrollHeight - 100) {
+            page.value++
+            fetchMovies()
+        }
+    }
 
     const filterMovies = computed(() => {
-        return movies.filter((movie) => {
-            const title = movie.title
+        return movies.value.filter((movie) => {
+            const title = movie.title || ''
             const dis = Hangul.disassemble(title).join('')
             const search = Hangul.disassemble(props.keyword).join('')
-    
-            // 일반 검색도 병행
             return (
                 title.toLowerCase().includes(props.keyword.toLowerCase()) ||
                 dis.includes(search)
---
+++ vueFlix/src/components/MovieItem.vue
@@ -0,0 +1,28 @@
+<template>
+    <li class="movie_item">
+        <img
+            :src="`https://image.tmdb.org/t/p/w500${movie.poster_path}`"
+            :alt="movie.title"
+        />
+        <p class="big sb">{{ movie.title }}</p>
+        <p class="text n">{{ genreNames.join(', ') }} | {{ releaseYear }}</p>
+    </li>
+</template>
+
+<script setup>
+    import { genreMap } from '../data/genres'
+
+    const props = defineProps({ movie: Object, genres: Object })
+
+    const releaseYear = props.movie.release_date
+    ? props.movie.release_date.slice(0, 4)
+    : '정보 없음'
+
+    const genreNames = props.movie.genre_ids
+  ? props.movie.genre_ids.map(id => genreMap[id] || '기타')
+  : []
+</script>
+
+<style scoped lang="scss">
+@import '../assets/movieitem.scss';
+</style>
