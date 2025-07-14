bb72b82edf36247e303198d931853cb446ed7480
woojin <77970494+woojin966@users.noreply.github.com>
2025-07-15 01:01:24 +0900

    메인배너 추가

--- vueFlix/src/App.vue
+++ vueFlix/src/App.vue
@@ -2,6 +2,7 @@
   <div class="body_wrap">
     <!-- $event : Header.vue에서 @update:keyword="$emit('update:keyword', $event)"로 보낸 값 - 입력한 키워드,을 keyword에 할당 -->
     <Header ref="headerRef" @update:keyword="keyword = $event"  v-model:keyword="keyword"></Header>
+    <MainBanner></MainBanner>
     <SearchResults v-if="keyword" :keyword="keyword" />
     <!-- Header에서 받아 할당한 keyword를 MoveList로 props보냄 -->
     <MoveList v-else :keyword="keyword" @notify="handleNotify"></MoveList>
@@ -12,6 +13,7 @@
 <script setup>
   import { ref } from 'vue'
   import Header from './components/Header.vue'
+  import MainBanner from './components/MainBanner.vue'
   import SearchResults from './components/SearchResults.vue'
   import MoveList from './components/MoveList.vue'
   import Footer from './components/Footer.vue'
---
+++ vueFlix/src/components/MainBanner.vue
@@ -0,0 +1,81 @@
+<template>
+    <article class="banner_article">
+        <!-- :autoplay="{ delay: 8000, disableOnInteraction: false }" -->
+        <Swiper
+            :modules="[Autoplay, Pagination]"
+            :loop="true"
+      
+            :pagination="{ clickable: true }"
+            class="main-banner-swiper"
+        >
+            <SwiperSlide v-for="movie in popularMoviesForBanner" :key="movie.id">
+                <section class="movie_info_section">
+                    <div>
+                        <div class="title_box">
+                            <h1 class="bb">{{ movie.title }}</h1>
+                            <p v-if="movie.krRelease" class="age_grade" :class="`age-${movie.krRelease}`">{{ movie.krRelease }}</p>
+                        </div>
+                        <p class="medium mb">{{ movie.overview || '설명이 없습니다.' }}</p>
+                    </div>
+                    <button class="mb text detail_btn">상세보기</button>
+                </section>
+                <section class="movie_poster_section">
+                    <img :src="posterUrlForBanner(movie.poster_path)" :alt="movie.title" />
+                </section>
+            </SwiperSlide>
+        </Swiper>
+    </article>
+</template>
+
+<script setup>
+    import { ref, onMounted } from 'vue'
+    import axios from 'axios'
+    import { Swiper, SwiperSlide } from 'swiper/vue'
+    import { Autoplay, Pagination } from 'swiper/modules'
+    import 'swiper/css'
+    import 'swiper/css/pagination'
+
+    const popularMoviesForBanner = ref([])
+
+    const fetchAgeRating = async (movieId) => {
+        try {
+            const res = await axios.get(`https://api.themoviedb.org/3/movie/${movieId}/release_dates`, {
+                params: {
+                api_key: 'fcc17cdaa94f35e5e1cf80b2de9ea4e7'
+            }
+        })
+        const krInfo = res.data.results.find(item => item.iso_3166_1 === 'KR')
+        if (!krInfo) return null
+        const release = krInfo.release_dates.find(r => r.certification)
+        return release?.certification || null
+        } catch (e) {
+            console.warn(`관람등급 가져오기 실패 (ID: ${movieId})`)
+            return null
+        }
+    }
+
+    const fetchpopularMovies = async () => {
+        const res = await axios.get('https://api.themoviedb.org/3/movie/popular', {
+            params: {
+                api_key: 'fcc17cdaa94f35e5e1cf80b2de9ea4e7',
+                language: 'ko-KR',
+            }
+        })
+        const movies = res.data.results.slice(0, 10) // 상위 5개만
+        const enriched = await Promise.all(
+            movies.map(async (movie) => {
+                const krRelease = await fetchAgeRating(movie.id)
+                return { ...movie, krRelease }
+            })
+        )
+        popularMoviesForBanner.value = enriched
+    }
+
+    const posterUrlForBanner = (path) => `https://image.tmdb.org/t/p/w500${path}`
+
+    onMounted(fetchpopularMovies)
+</script>
+
+<style scoped lang="scss">
+@import '../assets/mainbanner.scss';
+</style>
